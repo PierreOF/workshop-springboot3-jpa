9af7a0c5ac7b92f4d41e236e5c5acfe236954360
PierreOF <marcos.pierre@upe.br>
2023-11-07 08:58:21 -0300

    First commit

---
+++ src/main/java/com/pierremonteiro/SpringAndJpa/SpringAndJpaApplication.java
@@ -0,0 +1,13 @@
+package com.pierremonteiro.SpringAndJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndJpaApplication.class, args);
+	}
+
+}
